9084736a38326aae04101c4b551056a459aab8fd
Farnam <taheri.farnam@gmail.com>
2026-09-23 00:57:21 +0330

    feat(help): bind insert menu guidance

--- src/components/InsertMenu.tsx
+++ src/components/InsertMenu.tsx
@@ -17,7 +17,7 @@ export function InsertMenu({ disabled, onInsert }: InsertMenuProps) {
 
 
   return (
-    <div className="insert-menu" data-help-topic="toolbar">
+    <div className="insert-menu" data-help-topic="insert">
       <button
         ref={triggerRef}
         id={triggerId}
